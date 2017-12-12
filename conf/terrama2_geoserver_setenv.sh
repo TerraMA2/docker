9430c92be9e641d73736d07d22d3153ae2f5ff37
raphaelrpl <raphael.wcosta@gmail.com>
2017-12-12 09:07:13 -0200

    Update to 4.0.0

--- conf/terrama2_geoserver_setenv.sh
+++ conf/terrama2_geoserver_setenv.sh
@@ -3,4 +3,4 @@ export CATALINA_OPTS="$CATALINA_OPTS -server"
 export CATALINA_OPTS="$CATALINA_OPTS -Xms2048m"
 export CATALINA_OPTS="$CATALINA_OPTS -Xmx8192m"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:+UseParallelGC"
-export CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=America/Sao_Paulo
+export CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=America/Sao_Paulo"
