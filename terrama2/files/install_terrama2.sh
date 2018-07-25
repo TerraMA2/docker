2251c02dcfc4cb572a1ad77a04ee6577b75be0e6
raphaelrpl <raphael.wcosta@gmail.com>
2018-07-25 12:18:42 -0300

    TerraMA2 improvements: add internal ssh key in terrama2 image. Now it is not necessary to mount an external public and private key to webapp be able to connect without password.

--- terrama2/files/install_terrama2.sh
+++ terrama2/files/install_terrama2.sh
@@ -10,6 +10,8 @@ function valid()
   fi
 }
 
+apt-get update
+
 # TODO: Change URL to http://www.dpi.inpe.br/terrama2/doku.php?id=download and change curl parameter to -o deb_package_name
 echo -ne "Downloading TerraMA² ... "
 curl -O http://www.dpi.inpe.br/jenkins-data/terrama2/installers/linux/TerraMA2-${TERRAMA2_TAG}-release-linux-x64-Ubuntu-16.04.deb --silent
@@ -33,6 +35,8 @@ apt-get install -f -y
 if [ $? -ne 0 ]; then
   echo "Warning: Could not install TerraMA² Documentation Module"
 fi
+
+rm -rf /var/lib/apt/lists/*
 
 # No error
 exit 0
