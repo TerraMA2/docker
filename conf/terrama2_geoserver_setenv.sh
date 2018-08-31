c3633110c4a7296f598c410bee8ff00531af20f5
raphaelrpl <raphael.wcosta@gmail.com>
2018-08-31 16:12:02 -0300

    Set GeoServer timezone to GMT

--- conf/terrama2_geoserver_setenv.sh
+++ conf/terrama2_geoserver_setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CATALINA_OPTS="$CATALINA_OPTS -server"
-export CATALINA_OPTS="$CATALINA_OPTS -Xms2048m"
-export CATALINA_OPTS="$CATALINA_OPTS -Xmx8192m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xms1024m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx2048m"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:+UseParallelGC"
-export CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=America/Sao_Paulo"
+export CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=GMT"
