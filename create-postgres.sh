0aa136e105062ee73cd3c6b5fce285b5b3502ba1
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-05-28 16:55:50 -0300

    Fixed missing '&& \' on terrama2 Dockerfile

--- create-postgres.sh
+++ create-postgres.sh
@@ -2,9 +2,11 @@
 
 eval $(egrep -v '^#' .env | xargs)
 
+docker volume create terrama2_pg_vol
+
 docker run -d \
            --restart unless-stopped --name terrama2_pg \
-           -p 127.0.0.1:5433:5432 \
+           -p 127.0.0.1:5434:5432 \
            -v terrama2_pg_vol:/var/lib/postgresql/data \
            -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
            mdillon/postgis
