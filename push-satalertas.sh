5b1fc1c41925099b10e7861c8ec709bbd940f6fe
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-08-03 16:43:45 -0300

    Added 'release' to terrama2 deb name and added project name to the SatAlertas images

--- push-satalertas.sh
+++ push-satalertas.sh
@@ -5,11 +5,11 @@ _current_dir=${PWD}
 eval $(egrep -v '^#' .env | xargs)
 
 # TerraMA² SatAlertas server
-docker push ${TERRAMA2_DOCKER_REGISTRY}/satalertas-server:${SATALERTAS_TAG}
+docker push ${TERRAMA2_DOCKER_REGISTRY}/${TERRAMA2_PROJECT_NAME}-satalertas-server:${SATALERTAS_TAG}
 _terrama2_satalertas_server_code=$?
 
 # TerraMA² SatAlertas client
-docker push ${TERRAMA2_DOCKER_REGISTRY}/satalertas-client:${SATALERTAS_TAG}
+docker push ${TERRAMA2_DOCKER_REGISTRY}/${TERRAMA2_PROJECT_NAME}-satalertas-client:${SATALERTAS_TAG}
 _terrama2_satalertas_client_code=$?
 
 exit $(( ${_terrama2_satalertas_server_code} | ${_terrama2_satalertas_client_code} ))
