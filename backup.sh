51bea4f64731e36ebfe519a9c6c360fd3b6796a2
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-08-05 16:41:44 -0300

    Created restore backup script

--- backup.sh
+++ backup.sh
@@ -1,58 +1,114 @@
 #!/bin/bash
 
+echo ""
 echo "*******************"
 echo "* Backup TerraMA² *"
 echo "*******************"
 echo ""
 
-echo "**************"
-echo "* PostgreSQL *"
-echo "**************"
-echo ""
-
 eval $(egrep -v '^#' .env | xargs)
 
-DATE=$(date +%d-%m-%Y-%H:%M)
+DATE=$(date +%d-%m-%Y-%H-%M)
 
 if test -d "/var/lib/docker/volumes/terrama2_pg_vol/_data/"; then
+    echo ""
+    echo "**************"
+    echo "* PostgreSQL *"
+    echo "**************"
+    
     docker exec -it terrama2_pg bash -c "
+        echo \"\"; \
+        echo \"**************\"; \
+        echo \"* Backing up *\"; \
+        echo \"**************\"; \
+
+        echo \"\"; \
+        echo \"******************************\"; \
+        echo \"* Backing up schema terrama2 *\"; \
+        echo \"******************************\"; \
+        echo \"\"; \
+
         pg_dump -U postgres -h localhost -d ${POSTGRES_DATABASE} -n terrama2 -f /var/lib/postgresql/data/dump-terrama2-${DATE}.sql -v; \
+
+        echo \"\"; \
+        echo \"****************************\"; \
+        echo \"* Backing up schema public *\"; \
+        echo \"****************************\"; \
+        echo \"\"; \
+
         pg_dump -U postgres -h localhost -d ${POSTGRES_DATABASE} -n public -f /var/lib/postgresql/data/dump-public-${DATE}.sql -v; \
-        pg_dump -U postgres -h localhost -d ${POSTGRES_DATABASE} -n alertas -f /var/lib/postgresql/data/dump-alertas-${DATE}.sql -v; \
-        cd /var/lib/postgresql/data/; \
-        tar cvf - dump-terrama2-${DATE}.sql | gzip -9 - > dump-terrama2-${DATE}.tar.gz; \
-        tar cvf - dump-public-${DATE}.sql | gzip -9 - > dump-public-${DATE}.tar.gz; \
-        tar cvf - dump-alertas-${DATE}.sql | gzip -9 - > dump-alertas-${DATE}.tar.gz;
+
+        echo \"\"; \
+        echo \"*****************************\"; \
+        echo \"* Backing up schema alertas *\"; \
+        echo \"*****************************\"; \
+        echo \"\"; \
+
+        pg_dump -U postgres -h localhost -d ${POSTGRES_DATABASE} -n alertas -f /var/lib/postgresql/data/dump-alertas-${DATE}.sql -v
     "
+    
+    mkdir -vp ${BACKUP_DIR}/postgresql
 
-    mkdir -p ${BACKUP_DIR}/postgresql
+    echo ""
+    echo "********************"
+    echo "* Compacting dumps *"
+    echo "********************"
 
-    mv /var/lib/docker/volumes/terrama2_pg_vol/_data/dump-public-${DATE}.tar.gz ${BACKUP_DIR}/postgresql
-    mv /var/lib/docker/volumes/terrama2_pg_vol/_data/dump-terrama2-${DATE}.tar.gz ${BACKUP_DIR}/postgresql
+    cd /var/lib/docker/volumes/terrama2_pg_vol/_data/
+
+    echo ""
+    echo "***********************"
+    echo "* Compacting terrama2 *"
+    echo "***********************"
+    echo ""
+
+    tar cvf - dump-terrama2-${DATE}.sql | gzip -9 - > ${BACKUP_DIR}/postgresql/dump-terrama2-${DATE}.tar.gz
+
+    echo ""
+    echo "*********************"
+    echo "* Compacting public *"
+    echo "*********************"
+    echo ""
+
+    tar cvf - dump-public-${DATE}.sql | gzip -9 - > ${BACKUP_DIR}/postgresql/dump-public-${DATE}.tar.gz
+
+    echo ""
+    echo "**********************"
+    echo "* Compacting alertas *"
+    echo "**********************"
+    echo ""
+
+    tar cvf - dump-alertas-${DATE}.sql | gzip -9 - > ${BACKUP_DIR}/postgresql/dump-alertas-${DATE}.tar.gz
+    
+    rm -f dump-terrama2-${DATE}.sql
+    rm -f dump-public-${DATE}.sql
+    rm -f dump-alertas-${DATE}.sql
 fi
 
+echo ""
 echo "*************"
 echo "* Geoserver *"
 echo "*************"
 echo ""
 
 if test -d "/var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_geoserver_vol/_data/"; then
-    mkdir -p ${BACKUP_DIR}/geoserver
+    mkdir -vp ${BACKUP_DIR}/geoserver
 
-    docker exec -it ${TERRAMA2_PROJECT_NAME}_geoserver bash -c "cd /opt/geoserver/data_dir;tar cvf - * | gzip -9 - > geoserver-${DATE}.tar.gz"
+    cd /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_geoserver_vol/_data/
 
-    mv /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_geoserver_vol/_data/geoserver-${DATE}.tar.gz ${BACKUP_DIR}/geoserver
+    tar cvf - * | gzip -9 - > ${BACKUP_DIR}/geoserver/geoserver-${DATE}.tar.gz
 fi
 
+echo ""
 echo "**************"
 echo "* SatAlertas *"
 echo "**************"
 echo ""
 
 if test -d "/var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_satalertas_documents_vol/_data/"; then
-    mkdir -p ${BACKUP_DIR}/satalertas
+    mkdir -vp ${BACKUP_DIR}/satalertas
 
-    docker exec -it ${TERRAMA2_PROJECT_NAME}_satalertas_server_1 bash -c "cd documentos;tar cvf - *.pdf | gzip -9 - > satalertas_documents-${DATE}.tar.gz"
+    cd /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_satalertas_documents_vol/_data/
 
-    mv /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_satalertas_documents_vol/_data/satalertas_documents-${DATE}.tar.gz ${BACKUP_DIR}/satalertas
+    tar cvf - *.pdf | gzip -9 - > ${BACKUP_DIR}/satalertas/satalertas_documents-${DATE}.tar.gz
 fi
---
+++ restore.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+echo ""
+echo "********************"
+echo "* Restore TerraMA² *"
+echo "********************"
+echo ""
+
+eval $(egrep -v '^#' .env | xargs)
+
+if test -d "/var/lib/docker/volumes/terrama2_pg_vol/_data/"; then
+    echo ""
+    echo "**************"
+    echo "* PostgreSQL *"
+    echo "**************"
+    echo ""
+
+    cd ${BACKUP_DIR}/postgresql
+    
+    latestTerrama2BackupFile=$(basename -s .tar.gz $(ls -t1 | grep terrama2 | head -n 1))
+    latestAlertasBackupFile=$(basename -s .tar.gz $(ls -t1 | grep alertas | head -n 1))
+    latestPublicBackupFile=$(basename -s .tar.gz $(ls -t1 | grep public | head -n 1))
+
+    echo ""
+    echo "********************"
+    echo "* Extracting dumps *"
+    echo "********************"
+    echo ""
+
+    tar xvf ${latestTerrama2BackupFile}.tar.gz -C /var/lib/docker/volumes/terrama2_pg_vol/_data/
+    tar xvf ${latestAlertasBackupFile}.tar.gz -C /var/lib/docker/volumes/terrama2_pg_vol/_data/
+    tar xvf ${latestPublicBackupFile}.tar.gz -C /var/lib/docker/volumes/terrama2_pg_vol/_data/
+
+    docker exec -it terrama2_pg bash -c "
+        cd /var/lib/postgresql/data; \
+
+        echo ""
+        echo \"********************\"; \
+        echo \"* Dropping schemas *\"; \
+        echo \"********************\"; \
+        echo \"\"; \
+
+        psql -a -U postgres -h localhost -d ${POSTGRES_DATABASE} -c 'DROP SCHEMA terrama2 CASCADE'; \
+        psql -a -U postgres -h localhost -d ${POSTGRES_DATABASE} -c 'DROP TABLE alertas.reports CASCADE;DROP TABLE alertas.SequelizeMeta CASCADE;DROP SCHEMA alertas CASCADE'; \
+        psql -a -U postgres -h localhost -d ${POSTGRES_DATABASE} -c 'DROP SCHEMA public CASCADE'; \
+
+        echo ""
+        echo \"*******************\"; \
+        echo \"* Importing dumps *\"; \
+        echo \"*******************\"; \
+        echo \"\"; \
+
+        echo ""
+        echo \"*****************************\"; \
+        echo \"* Importing schema terrama2 *\"; \
+        echo \"*****************************\"; \
+        echo \"\"; \
+
+        psql -a -U postgres -h localhost -d ${POSTGRES_DATABASE} < ${latestTerrama2BackupFile}.sql; \
+
+        echo ""
+        echo \"****************************\"; \
+        echo \"* Importing schema alertas *\"; \
+        echo \"****************************\"; \
+        echo \"\"; \
+
+        psql -a -U postgres -h localhost -d ${POSTGRES_DATABASE} < ${latestAlertasBackupFile}.sql; \
+
+        echo ""
+        echo \"***************************\"; \
+        echo \"* Importing schema public *\"; \
+        echo \"***************************\"; \
+        echo \"\"; \
+
+        psql -a -U postgres -h localhost -d ${POSTGRES_DATABASE} < ${latestPublicBackupFile}.sql; \
+
+        echo ""
+        echo \"******************\"; \
+        echo \"* Removing dumps *\"; \
+        echo \"******************\"; \
+        echo \"\"; \
+
+        rm -vf ${latestTerrama2BackupFile}.sql; \
+        rm -vf ${latestAlertasBackupFile}.sql; \
+        rm -vf ${latestPublicBackupFile}.sql;
+    "
+fi
+
+if test -d "/var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_geoserver_vol/_data/"; then
+    echo ""
+    echo "*************"
+    echo "* Geoserver *"
+    echo "*************"
+    echo ""
+
+    cd ${BACKUP_DIR}/geoserver
+
+    latestGeoserverBackupFile=$(ls -t1 | head -n 1)
+
+    echo ""
+    echo "*********************"
+    echo "* Removing old data *"
+    echo "*********************"
+    echo ""
+
+    rm -vrf /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_geoserver_vol/_data/*
+
+    echo ""
+    echo "**************"
+    echo "* Extracting *"
+    echo "**************"
+    echo ""
+
+    tar xvf ${latestGeoserverBackupFile} -C /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_geoserver_vol/_data/
+fi
+
+if test -d "/var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_satalertas_documents_vol/_data/"; then
+    echo ""
+    echo "**************"
+    echo "* SatAlertas *"
+    echo "**************"
+    echo ""
+
+    cd ${BACKUP_DIR}/satalertas
+
+    latestDocumentBackupFile=$(ls -t1 | head -n 1)
+    echo ""
+    echo "*********************"
+    echo "* Removing old data *"
+    echo "*********************"
+    echo ""
+
+    rm -vrf /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_satalertas_documents_vol/_data/*
+
+    echo ""
+    echo "**************"
+    echo "* Extracting *"
+    echo "**************"
+    echo ""
+
+    tar xvf ${latestDocumentBackupFile} -C /var/lib/docker/volumes/${TERRAMA2_PROJECT_NAME}_satalertas_documents_vol/_data/
+fi
