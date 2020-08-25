56f0d44bb1b2b78fb20cc7a29dc0451b9fcd054f
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-08-24 22:41:00 -0300

    Fixed postgres password on create-postgres.sh

--- create-postgres.sh
+++ create-postgres.sh
@@ -13,5 +13,5 @@ docker run -d \
            --restart unless-stopped --name terrama2_pg \
            -p 127.0.0.1:5433:5432 \
            -v terrama2_pg_vol:/var/lib/postgresql/data \
-           -e POSTGRES_PASSWORD=${POSTGRES_DATABASE} \
+           -e POSTGRES_PASSWORD="postgres" \
            mdillon/postgis
