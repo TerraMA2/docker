99d9e3d237ac92efe1a57d248d09430e301b1233
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-08-31 20:47:16 -0300

    Added services configuration to settings.json

--- configure-version.sh
+++ configure-version.sh
@@ -32,6 +32,7 @@ for image in conf/terrama2_webapp_settings.json.in \
         -e 's!%%TERRAMA2_DOCKER_REGISTRY%%!'"${TERRAMA2_DOCKER_REGISTRY}"'!g' \
         -e 's!%%TERRAMA2_TAG%%!'"${TERRAMA2_TAG}"'!g' \
         -e 's!%%SATALERTAS_TAG%%!'"${SATALERTAS_TAG}"'!g' \
+        -e 's!%%TERRAMA2_PROTOCOL%%!'"${TERRAMA2_PROTOCOL}"'!g' \
         -e 's!%%TERRAMA2_DNS%%!'"${TERRAMA2_DNS}"'!g' \
         -e 's!%%TERRAMA2_BASE_PATH%%!'"${TERRAMA2_BASE_PATH}"'!g' \
         -e 's!%%TERRAMA2_WEBAPP_ADDRESS%%!'"${TERRAMA2_WEBAPP_ADDRESS}"'!g' \
@@ -44,5 +45,6 @@ for image in conf/terrama2_webapp_settings.json.in \
         -e 's!%%PUBLIC_URI%%!'"${PUBLIC_URI}"'!g' \
         -e 's!%%WEBMONITOR_BASE_PATH%%!'"${WEBMONITOR_BASE_PATH}"'!g' \
         -e 's!%%WEBAPP_BASE_PATH%%!'"${WEBAPP_BASE_PATH}"'!g' \
+        -e 's!%%GEOSERVER_PORT%%!'"${GEOSERVER_PORT}"'!g' \
       "${image}" > "${image::-3}"
 done
