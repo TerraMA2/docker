b98d7aebd22e6b5876a3bdae9dc329b8e43a9807
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-11-25 08:09:52 -0300

    Upgraded geoserver version to 2.18

--- backup.sh
+++ backup.sh
@@ -21,7 +21,7 @@ if test -d "/var/lib/docker/volumes/terrama2_pg_vol/_data/"; then
     echo "* Backing up database ${POSTGRES_DATABASE} *"
     echo "********************************************"
 
-    docker exec -it terrama2_pg bash -c "pg_dump -U postgres -h localhost -d ${POSTGRES_DATABASE} -f /var/lib/postgresql/data/dump-${DATE}.sql -v"
+    docker exec -it terrama2_pg bash -c "pg_dump -f /var/lib/postgresql/data/dump-${DATE}.sql -h localhost -p 5432 -U postgres --quote-all-identifiers --no-password -v --inserts --role=postgres -Fp -b -c -C ${POSTGRES_DATABASE}"
 
     mkdir -vp ${BACKUP_DIR}/postgresql
 
--- create-postgres.sh
+++ create-postgres.sh
@@ -11,7 +11,7 @@ docker volume create terrama2_pg_vol
 
 docker run -d \
            --restart unless-stopped --name terrama2_pg \
-           -p 127.0.0.1:5433:5432 \
+           -p 127.0.0.1:5435:5432 \
            -v terrama2_pg_vol:/var/lib/postgresql/data \
            -e POSTGRES_PASSWORD="postgres" \
            postgis/postgis:11-2.5
