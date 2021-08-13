303090bc875d766736687f57692d19c32d507441
Marcelo Pilatti <marcelo.silva979@gmail.com>
2021-08-13 17:13:17 -0300

    Added docker swarm entries on docker-compose.yml

--- configure-version.sh
+++ configure-version.sh
@@ -20,11 +20,14 @@ for image in conf/terrama2_webapp_settings.json.in \
              conf/terrama2_webmonitor.json.in \
              terrama2/Dockerfile.in \
              webapp/Dockerfile.in \
+             docker-compose.yml.in \
              webmonitor/Dockerfile.in; do
   sed -r \
         -e 's!%%COMPOSE_PROJECT_NAME%%!'"${COMPOSE_PROJECT_NAME}"'!g' \
         -e 's!%%TERRAMA2_DOCKER_REGISTRY%%!'"${TERRAMA2_DOCKER_REGISTRY}"'!g' \
         -e 's!%%TERRAMA2_TAG%%!'"${TERRAMA2_TAG}"'!g' \
+        -e 's!%%TERRAMA2_DATA_DIR%%!'"${TERRAMA2_DATA_DIR}"'!g' \
+        -e 's!%%TERRAMA2_CONFIG_DIR%%!'"${TERRAMA2_CONFIG_DIR}"'!g' \
         -e 's!%%TERRAMA2_PROTOCOL%%!'"${TERRAMA2_PROTOCOL}"'!g' \
         -e 's!%%TERRAMA2_DNS%%!'"${TERRAMA2_DNS}"'!g' \
         -e 's!%%TERRAMA2_BASE_PATH%%!'"${TERRAMA2_BASE_PATH}"'!g' \
